6a32a514b9986a26c091b2cd9138be5061e20ea2
Piotr Socko <socko.piotr@o2.pl>
2023-03-31 00:14:39 +0200

    Basic Physics made by changing style of DOM element

--- ponggame/src/Computer.js
+++ ponggame/src/Computer.js
@@ -11,7 +11,7 @@ export default function Computer() {
  
      window.addEventListener('mousemove', handleMouseMove);
 
-    //  new Ball(window.innerWidth/2,window.innerHeight/2,{x:-9,y:-5})
+     new Ball(window.innerWidth/2,window.innerHeight/2,{x:-9,y:-5})
 
      return () => {
        window.removeEventListener(
@@ -26,33 +26,33 @@ export default function Computer() {
    else if(MousePosition.x >= window.innerWidth-150 ) center = window.innerWidth - 300
    else center = MousePosition.x - 150
 
-  //  class Ball{
-  //   constructor(x,y,speedvector){
-  //     this.X = x;
-  //     this.Y = y;
-  //     this.vectorX = speedvector.x;
-  //     this.vectorY = speedvector.y;
-  //     setBallPosition({x:this.X,y:this.Y})
-  //     this.time = Date.now()
-  //     this.draw()
-  //   }
-  //   draw(){
+   class Ball{
+    constructor(x,y,speedvector){
+      this.X = x;
+      this.Y = y;
+      this.vectorX = speedvector.x;
+      this.vectorY = speedvector.y;
+      setBallPosition({x:this.X,y:this.Y})
+      this.time = Date.now()
+      this.draw()
+    }
+    draw(){
       
-  //     setTimeout(() => {
-  //       let ctime = Date.now()  
-  //       let diffx = this.X + ((ctime - this.time)/100) * this.vectorX
-  //       if(this.X - diffx>1) console.log(this.X +10 < this.X + ((ctime - this.time)/100) * this.vectorX)
-  //       this.X = this.X + ((ctime - this.time)/100) * this.vectorX
-  //       this.Y = this.Y + ((ctime - this.time)/100) * this.vectorY
-  //       setBallPosition({x:this.X, y:this.Y})
+      setTimeout(() => {
+        let ctime = Date.now()  
+        let diffx = this.X + ((ctime - this.time)/100) * this.vectorX
+        if(this.X - diffx>1) console.log(this.X +10 < this.X + ((ctime - this.time)/100) * this.vectorX)
+        this.X = this.X + ((ctime - this.time)/100) * this.vectorX
+        this.Y = this.Y + ((ctime - this.time)/100) * this.vectorY
+        setBallPosition({x:this.X, y:this.Y})
        
-  //       this.time = Date.now() 
+        this.time = Date.now() 
         
-  //       this.draw()
-  //     }, 16);
+        this.draw()
+      }, 16);
       
-  //   }
-  //  }
+    }
+   }
 
    
 
@@ -60,7 +60,7 @@ export default function Computer() {
     <>
     
     <div className='Computer container'>
-      {/* <div className="ball" style={{ left: BallPosition.x + "px" ,bottom:BallPosition.y+ "px"}}>  </div> */}
+      <div className="ball" style={{ left: BallPosition.x + "px" ,bottom:BallPosition.y+ "px"}}>  </div>
       <div className="myPlatform" style={{ left: center + "px"}}>  </div>
     </div>
     </>
